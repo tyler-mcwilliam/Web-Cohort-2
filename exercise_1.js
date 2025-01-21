aed3bec3f75e4257cf82e392ecef62fce6053ec8
Alice Lou <Alice.kc.lou@gmail.com>
2025-01-21 20:04:48 +0000

    resubmit

--- exercise_1.js
+++ exercise_1.js
@@ -1,3 +1,4 @@
+//resubmit
 // Web Cohort 1 Exercise 1
 
 // For each expression, predict what you think the output will be in a comment (`//`) ***without first running the command***. Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
