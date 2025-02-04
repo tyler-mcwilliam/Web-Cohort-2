58bccb372bc7cc90d73dbc5905802a8fa66cec05
Tyler McWilliam <tyler.p.mcwilliam@altria.com>
2025-02-04 13:00:10 -0600

    Create exercise_3.js

---
+++ exercise_3.js
@@ -0,0 +1,6 @@
+// DO NOT COMMIT ANY CREDENTIALS!!!
+
+// Using pseudocode, explain in plain English how you would use the Google Maps API to receive directions from Altria HQ to the nearest gas station. Assume you know the address of Altria HQ, but not the address of the nearest gas station
+
+
+// Challenge - Extreme: Write a function in Javascript that executes this process, taking the API key as an argument. DO NOT COMMIT THE API KEY.
